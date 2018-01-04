e6b1c50c367bb6fab26d9b179b62987163c39881
edgon <edgon85@gmail.com>
2018-01-04 15:00:59 -0600

    1 - crado los componentes y servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graphics1Component } from './pages/graphics1/graphics1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graphics1Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule
