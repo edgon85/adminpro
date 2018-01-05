d5bfdbae016d1b68f15793f7c80b42260257e5e0
edgon <edgon85@gmail.com>
2018-01-05 01:19:15 -0600

    cambios en modulos y rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,15 +10,8 @@ import { PagesModule } from './pages/pages.modules';
 // components
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graphics1Component } from './pages/graphics1/graphics1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 
 
@@ -28,13 +21,6 @@ import { RegisterComponent } from './login/register.component';
     LoginComponent,
     RegisterComponent,
     NopagefoundComponent,
-    // DashboardComponent,
-    // ProgressComponent,
-    // Graphics1Component,
-    // PagesComponent,
-    // HeaderComponent,
-    // SidebarComponent,
-    // BreadcrumbsComponent,
   ],
   imports: [
     BrowserModule,
