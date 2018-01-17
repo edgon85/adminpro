f54cb679f69b4de1e75fdf613a0f9ada67fb9bae
edgon <edgon85@gmail.com>
2018-01-17 12:43:53 -0600

    seccion de servicios agregados, configuracion routerLink de sidebar usando services

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
     declarations: [
@@ -13,6 +16,10 @@ import { SidebarComponent } from './sidebar/sidebar.component';
         HeaderComponent,
         SidebarComponent,
         BreadcrumbsComponent
+    ],
+    imports: [
+        RouterModule,
+        CommonModule
     ]
 })
 
