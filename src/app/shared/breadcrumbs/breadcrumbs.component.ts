6174575c605d4c1d4e9813f0d4e5e3fa9bcba747
edgon <edgon85@gmail.com>
2018-01-18 01:23:18 -0600

    MetaTags, Título y página actual, log comentados

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivationEnd } from '@angular/router';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -8,20 +9,37 @@ import { Router, ActivationEnd } from '@angular/router';
 })
 export class BreadcrumbsComponent implements OnInit {
 
-  constructor( private router: Router) {
+  label: string = '';
 
-    this.router.events
-      .filter( evento => evento instanceof ActivationEnd )
-      .filter( (evento: ActivationEnd) => evento.snapshot.firstChild === null )
-      .map( (evento: ActivationEnd) => evento.snapshot.data)
-      .subscribe( event => {
+  constructor(
+      private router: Router,
+      public title: Title,
+      public meta: Meta
+      ) {
 
-        console.log( event );
+    this.getDataRoute()
+    .subscribe( data => {
 
-      });
+      // console.log( data );
+      this.label = data.titulo;
+      this.title.setTitle( this.label );
+
+      let metaTag: MetaDefinition = {
+        name: 'description',
+        content: this.label
+      };
+      this.meta.updateTag( metaTag );
+
+    });
   }
 
   ngOnInit() {
   }
 
+  getDataRoute() {
+    return  this.router.events
+    .filter( evento => evento instanceof ActivationEnd )
+    .filter( (evento: ActivationEnd) => evento.snapshot.firstChild === null )
+    .map( (evento: ActivationEnd) => evento.snapshot.data);
+  }
 }
