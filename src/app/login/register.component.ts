04f8886ab954d6733f7827e6584122b902623b5d
edgon <edgon85@gmail.com>
2018-03-24 12:22:24 -0600

    Creando usuarios en el frontend, en register con el formulario

--- src/app/login/register.component.ts
+++ src/app/login/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 declare function init_plugins();
 
@@ -9,10 +10,24 @@ declare function init_plugins();
 })
 export class RegisterComponent implements OnInit {
 
+  forma: FormGroup;  // trabaja el formulario de registro
+
   constructor() { }
 
   ngOnInit() {
     init_plugins(); // inicia el plugin
+
+    this.forma = new FormGroup({
+      username: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, Validators.required),
+      password2: new FormControl(null, Validators.required),
+      condiciones: new FormControl(false)
+    });
+  }
+
+  registrarUsuario() {
+    console.log(this.forma.value);
   }
 
 }
--- src/app/models/usuario.model.ts
+++ src/app/models/usuario.model.ts
@@ -1,4 +1,12 @@
 
 export class Usuario {
-    constructor () {}
+    constructor (
+        public username: string,
+        public email: string,
+        public password: string,
+        public password2: string,
+        public img?: string,
+        public role?: string,
+        public _id?: string
+    ) {}
 }
